0fba7af556b6d040a949577020216316de1576e8
xiangqianma <xiangqianma@outlook.com>
2019-12-01 10:03:45 +0800

    delete resize[300, 300] in val dataset

--- datasets/create_dataset.py
+++ datasets/create_dataset.py
@@ -97,8 +97,7 @@ class ValDataset(Dataset):
         image = Image.open(sample_path).convert('RGB')
         label = self.label_list[index]
         transform_val_list = [ 
-                    T.Resize([300, 300], interpolation=3),
-                    T.CenterCrop(self.size),
+                    T.Resize(self.size, interpolation=3),
                     T.ToTensor(),
                     T.Normalize(self.mean, self.std)
                 ]          
